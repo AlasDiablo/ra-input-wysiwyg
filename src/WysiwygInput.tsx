b3de300221a2cb42f9fe3a35cbedb205696c6d2e
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-07-05 11:27:20 +0200

    feat: making working !

--- src/WysiwygInput.tsx
+++ src/WysiwygInput.tsx
@@ -1,6 +1,6 @@
 import FormHelperText from '@mui/material/FormHelperText';
 import clsx from 'clsx';
-import { convertToRaw } from 'draft-js';
+import { EditorState, ContentState, convertToRaw } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from 'html-to-draftjs';
 import PropTypes from 'prop-types';
@@ -8,11 +8,52 @@ import { useInput } from 'ra-core';
 import { Labeled, InputHelperText } from 'ra-ui-materialui';
 // eslint-disable-next-line import/no-namespace,@typescript-eslint/no-unused-vars
 import * as React from 'react';
-import { useCallback, useEffect, useState } from 'react';
-import { ContentState, Editor, EditorState } from 'react-draft-wysiwyg';
+import { Component, memo } from 'react';
+import { Editor } from 'react-draft-wysiwyg';
 import type { TextInputProps } from 'ra-ui-materialui';
+import type { ControllerRenderProps } from 'react-hook-form';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 export type WysiwygInputProps = TextInputProps;
+
+class WysiwygInputClass extends Component<ControllerRenderProps, { editorState: EditorState }> {
+    constructor(props: ControllerRenderProps) {
+        super(props);
+        const contentBlock = htmlToDraft(props.value);
+        if (contentBlock) {
+            const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
+            const editorState = EditorState.createWithContent(contentState);
+            this.state = {
+                editorState,
+            };
+        }
+    }
+
+    onEditorStateChange = (editorState) => {
+        this.setState({
+            editorState,
+        });
+        const { onChange, onBlur } = this.props;
+        // eslint-disable-next-line react/destructuring-assignment
+        onChange(draftToHtml(convertToRaw(editorState.getCurrentContent())));
+        onBlur();
+    };
+
+    render() {
+        const { editorState } = this.state;
+        return (
+            <div>
+                <Editor
+                    editorState={editorState}
+                    wrapperClassName="wysiwyg-wrapper"
+                    editorClassName="wysiwyg-editor"
+                    onEditorStateChange={this.onEditorStateChange}
+                />
+                <textarea disabled value={draftToHtml(convertToRaw(editorState.getCurrentContent()))} />
+            </div>
+        );
+    }
+}
 
 export const WysiwygInput = ({
     className,
@@ -47,27 +88,6 @@ export const WysiwygInput = ({
         ...rest,
     });
 
-    const [editorState, setEditorState] = useState<EditorState | undefined>();
-
-    useEffect(() => {
-        const contentBlock = htmlToDraft(field.value);
-        if (contentBlock) {
-            const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
-            setEditorState(EditorState.createWithContent(contentState));
-            return;
-        }
-        setEditorState(EditorState.createEmpty());
-    }, [field.value]);
-
-    const handleEditorStateChange = useCallback(
-        (state: EditorState) => {
-            setEditorState(state);
-            field.onChange(draftToHtml(convertToRaw(state.getCurrentContent())));
-            field.onBlur();
-        },
-        [field],
-    );
-
     const renderHelperText = helperText !== false || ((isTouched || isSubmitted) && invalid);
     const isError = (isTouched || isSubmitted) && invalid;
 
@@ -82,7 +102,7 @@ export const WysiwygInput = ({
                 resource={resource}
                 fullWidth
             >
-                <Editor editorState={editorState} onEditorStateChange={handleEditorStateChange} />
+                <WysiwygInputClass {...field} />
             </Labeled>
             {renderHelperText ? (
                 <FormHelperText error={isError}>
@@ -108,3 +128,5 @@ WysiwygInput.propTypes = {
 WysiwygInput.defaultProps = {
     options: {},
 };
+
+export default memo(WysiwygInput);
